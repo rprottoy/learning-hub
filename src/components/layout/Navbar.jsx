7fc7a8d47193a2cd61e8915226d366ad4df4832d
Resalat Al Samin <risalat731@gmail.com>
2026-03-04 14:34:08 +0600

    checking git is working

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="fixed top-0 left-0 w-full bg-white shadow-md z-50"></nav>
+    </div>
+  );
+};
+
+export default Navbar;
